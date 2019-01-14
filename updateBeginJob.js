5833abee8e04c40dd32360d09ebc72de3bca6319
duytan8484 <43599675+duytan8484@users.noreply.github.com>
2019-01-14 14:27:43 +0700

    Add files via upload

---
+++ updateBeginJob.js
@@ -0,0 +1,29 @@
+var fs = require('fs');
+var request = require('request');
+
+function updateStatus(endpoint, configuration, status, callback) {
+    var url = endpoint + '/api/jobs/' + configuration.jobId + '/' + status;
+    request({
+        url: url,
+        method: 'POST',
+        json: true,
+        body: configuration,
+        headers: {
+            'x-api-key': '0I_xh&qh1?,1DBz>lZ]bIwNisc,aB83+_XaC):7M!cG6w:dRK}]Q}dCE9VIijq>f'
+        }
+    }, callback);
+}
+
+var riverflowEndpoint = process.env.RIVERFLOWENDPOINT;
+var riverflowConfiguration = JSON.parse(process.env.RIVERFLOWCONFIGURATION);
+
+// Update link for to console log
+riverflowConfiguration.consoleLog = "   ";
+
+updateStatus(riverflowEndpoint, riverflowConfiguration, 'InProgress', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        // Do nothing here
+    } else {
+        console.log(error);
+    }
+});
